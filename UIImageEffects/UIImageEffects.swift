e20d55467cb63a9546bf607e1d868091f192f479
Hong Xin <hongxinhope@gmail.com>
2015-12-05 15:32:32 +0800

    Initial Commit

---
+++ UIImageEffects/UIImageEffects.swift
@@ -0,0 +1,13 @@
+//
+//  UIImageEffects.swift
+//  UIImageEffects
+//
+//  Created by 洪鑫 on 15/12/5.
+//  Copyright © 2015年 Xin Hong. All rights reserved.
+//
+
+import UIKit
+
+public extension UIImage {
+    
+}
